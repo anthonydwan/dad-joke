6814fc414f6be898153a0e82a264dac70af4d6a7
Anthony <anthonydwan@users.noreply.github.com>
2020-06-27 15:06:49 +0800

    updated loop

---
+++ dad_joker.py
@@ -0,0 +1,79 @@
+import requests
+import random
+import pyfiglet
+import time
+import sys
+
+def slow_text(slow_text):
+    for char in slow_text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print('\n')
+
+url =  'https://icanhazdadjoke.com/search'
+
+title = pyfiglet.figlet_format("DAD JOKE MACHINE!")
+print(title)
+slow_text("Welcome to Dad Joke Machine.")
+slow_text("You just have to type a topic out,")
+slow_text("and I'll give you a silly joke!")
+
+def retry():
+  slow_text('You do want to try again? Enter [Y]/[N]')
+  again = input()
+  if again == 'Y':
+    dad_joker()
+  elif again == 'N':
+    slow_text('Cya!')
+  else:
+    slow_text('Wrong input!')
+    retry()
+
+def dad_joker():
+  """a function for fetching dad joke"""
+
+  slow_text("\nGimme a topic: ")
+  user_input = input()
+
+  response = requests.get(url, headers={'Accept':'Application/json'}, params={"term": user_input}).json()
+
+  results = response['results']
+  total_jokes = response['total_jokes']
+
+  def rejoke():
+    slow_text("do you want another? Enter [Y]/[N]")
+    joke2 = input()
+    if joke2 == 'Y':
+      slow_text(f"on '{user_input}' again? Enter [Y]/[N]")
+      cont = input()
+      if cont == 'Y':
+        slow_text(results[random.randrange(0,total_jokes)]['joke'])
+        rejoke()
+      elif cont == 'N':
+        dad_joker()
+      else:
+        slow_text("Wrong input!")
+        rejoke()
+    elif joke2 == 'N':
+      slow_text('Cya!')
+    else:
+      slow_text('Wrong input!')
+      rejoke()
+
+
+  if total_jokes==0:
+    slow_text(f"There's nothing on the topic '{user_input}'!")
+    retry()
+
+  elif total_jokes==1:
+    slow_text(f"There's one joke on the topic '{user_input}'. Let me tell you:")
+    slow_text(results[0]['joke'])
+
+  else:
+    slow_text(f"There are {total_jokes} jokes on the topic '{user_input}'. Let me pick a good one for you:")
+    slow_text(results[random.randrange(0,total_jokes)]['joke'])
+    rejoke()
+
+
+dad_joker()
